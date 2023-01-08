9bd15d40dbdc12693389bebc7e6c3b749f880bcc
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2023-01-08 02:12:47 +0100

    day1 (first part) solved

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    // let r = include_str!("./day1.test");
+    let r = include_str!("./day1.prod");
+    let v: Vec<&str> = r.split("\n\n").collect();
+    let elves: Vec<i32> = v.iter().map(|elv| {
+        elv
+            .split("\n")
+            .map(|s| s.parse::<i32>().unwrap_or(0))
+            .sum::<i32>()
+    }).collect();
+
+    let max = elves.iter().max().unwrap();
+
+    println!("{:?}", max);
+
+    Ok(())
 }
